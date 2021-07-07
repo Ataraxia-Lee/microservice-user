b74a4dee69e7bdd51e3306dfe54c8c514c59fd8d
lilong <1349064245@qq.com>
2021-07-07 11:41:08 +0800

    提交文件

---
+++ src/main/java/com/ataraxia/microservices/MicroserviceUserApplication.java
@@ -0,0 +1,16 @@
+package com.ataraxia.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author MyPC
+ */
+@SpringBootApplication
+public class MicroserviceUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceUserApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/ataraxia/microservices/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.ataraxia.microservices.controller;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author MyPC
+ */
+@RestController
+public class UserController {
+
+    @GetMapping("/index")
+    public List<String> index() {
+        SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        List<String> list = new ArrayList<>();
+        list.add("1");
+        return list;
+    }
+
+}
