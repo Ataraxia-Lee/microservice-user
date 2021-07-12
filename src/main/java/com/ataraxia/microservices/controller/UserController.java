0793e47170a2a7695cc4c1e43239a53f0dbdf1cf
lilong <1349064245@qq.com>
2021-07-12 10:13:24 +0800

    knife文档操作工具类

--- src/main/java/com/ataraxia/microservices/controller/UserController.java
+++ src/main/java/com/ataraxia/microservices/controller/UserController.java
@@ -4,7 +4,6 @@ import com.ataraxia.microservices.util.response.Result;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
